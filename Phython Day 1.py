a84cfc8b0f74b008d40be9d2cdc7a2afb4816ef5
Mithuniliyanwalage <mithuliyanwalage@gmail.com>
2026-05-16 21:07:47 +0530

    Added python day-1 code

---
+++ Phython Day 1.py
@@ -0,0 +1,13 @@
+m1=int(input("Enter a mark for student 1;"))
+m2=int(input("Enter a mark for student 2;"))
+m3=int(input("Enter a mark for student 3;"))
+m4=int(input("Enter a mark for student 4;"))
+m5=int(input("Enter a mark for student 5;"))
+average=(m1+m2+m3+m4+m5)/5
+print("Average marks=",average)
+
+r=float(input("Enter a value for radius:"))
+Perimeter=(2*3.142857143*r)
+Area=(3.142857143*r*r)
+print("Perimeter equals:",Perimeter)
+print("Area equals:",Area)
