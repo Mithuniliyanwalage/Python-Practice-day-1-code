487a9b3e0b1f67924b9e8a72deafd457bd00c033
Mithuniliyanwalage <mithuliyanwalage@gmail.com>
2026-05-16 22:12:05 +0530

    Phython practice day-1

--- Phython Day 1.py
+++ Phython Day 1.py
@@ -11,3 +11,17 @@ Perimeter=(2*3.142857143*r)
 Area=(3.142857143*r*r)
 print("Perimeter equals:",Perimeter)
 print("Area equals:",Area)
+
+p=float(input("Enter a principle amount:"))
+r=float(input("Enter an interest rate:"))
+y=int(input("Enter no. of years:"))
+Compound_Interest=p*((100+r)/100)**p
+print("Compound Interest is equals to:",Compound_Interest)
+
+b=float(input("Enter basic salary:"))
+d=int(input("Enter no. of working days:"))
+h=int(input("Enter extra hours:"))
+total=(d*(b*0.05))+h*250
+tax=total*0.12
+Salary=total-tax
+print("Salary is:",Salary)
